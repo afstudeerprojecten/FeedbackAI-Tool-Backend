66b26d399e51f1aa6173a8cdf4daffa45e29415e
AlecV <alec.vangilbergen@student.ucll.be>
2024-03-19 11:37:32 +0100

    Added pydantic models to schemas.py

---
+++ Project/app/schemas.py
@@ -0,0 +1,103 @@
+from pydantic import BaseModel
+from typing import Optional, List
+
+
+# Command models for creating/updating data
+class CreateOrganisation(BaseModel):
+    name: str
+
+
+class CreateTeacher(BaseModel):
+    name: str
+    lastname: str
+    email: str
+    password: str
+    organisation_id: int
+
+
+class CreateStudent(BaseModel):
+    name: str
+    lastname: str
+    email: str
+    password: str
+    organisation_id: int
+
+
+# Query models for retrieving data
+class Organisation(BaseModel):
+    id: int
+    name: str
+
+    class Config:
+        orm_mode = True
+
+
+class Teacher(BaseModel):
+    id: int
+    name: str
+    lastname: str
+    email: str
+    password: str
+    organisation_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class Student(BaseModel):
+    id: int
+    name: str
+    lastname: str
+    email: str
+    password: str
+    organisation_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class Course(BaseModel):
+    id: int
+    name: str
+    teacher_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class Assignment(BaseModel):
+    id: int
+    name: str
+    teacher_id: int
+    templates: List["Template"] = []
+
+    class Config:
+        orm_mode = True
+
+
+class Template(BaseModel):
+    id: int
+    content: str
+    assignment_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class Submission(BaseModel):
+    id: int
+    content: str
+    assignment_id: int
+    student_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class Feedback(BaseModel):
+    id: int
+    content: str
+    submission_id: int
+
+    class Config:
+        orm_mode = True
