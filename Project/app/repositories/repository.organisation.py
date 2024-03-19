0439f74b50a3efd6fc536a9218d710c0604d9d6a
AlecV <alec.vangilbergen@student.ucll.be>
2024-03-19 16:39:02 +0100

    updated organisation repository

--- Project/app/repositories/repository.organisation.py
+++ Project/app/repositories/repository.organisation.py
@@ -1,10 +1,14 @@
 from dataclasses import dataclass    
 import app.models as models
-from app.database import Engine
+from app.database import engine
 from app.schemas import CreateOrganisation
 from sqlalchemy.orm import Session
 from passlib.context import CryptContext
 from typing import Protocol
+from sqlalchemy import Engine
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 
 class OrganisationRepositoryInterface(Protocol):
     def create_organisation(self, organisation: CreateOrganisation) -> models.Organisation:
@@ -18,11 +22,14 @@ class OrganisationRepository:
         self.engine = engine
 
     def create_organisation(self, organisation: CreateOrganisation) -> models.Organisation:
+        hashed_password = pwd_context.hash(organisation.password)
         with self.engine.connect() as conn:
             conn.execute(
                 models.Organisation.__table__.insert().values(
-                    name=organisation.name
+                    name=organisation.name,
+                    username=organisation.username,
+                    password=hashed_password
                 )
             )
             conn.commit()
-        return models.Organisation(name=organisation.name)
+        return models.Organisation(name=organisation.name, username=organisation.username, password=hashed_password)
