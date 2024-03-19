a4ee8c94a34ae6b03f132e83fbf8d49cf407db80
AlecV <alec.vangilbergen@student.ucll.be>
2024-03-19 10:36:08 +0100

    Added initial file structure

---
+++ Project/app/database.py
@@ -0,0 +1,18 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Determine the database URL based on the environment
+if os.getenv("APP_ENV") == "production":
+    # Use local database connection
+    URL_DATABASE = 'postgresql://postgres:postgres@postgres-db:5432/postgres'
+else :
+    URL_DATABASE = 'postgresql://postgres:postgres@localhost:5432/postgres'
+
+# Create SQLAlchemy engine and sessionmaker
+engine = create_engine(URL_DATABASE)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Create declarative base
+Base = declarative_base()
---
+++ Project/app/models.py
@@ -0,0 +1,5 @@
+from enum import Enum
+from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.types import Enum as SQLAlchemyEnum
+import os
+from app.database import Base
