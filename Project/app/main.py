80d2c523fa59e35d19b60407eb6d9d03019169ce
AlecV <alec.vangilbergen@student.ucll.be>
2024-03-19 14:03:08 +0100

    Add FastAPI endpoint and database connection setup

---
+++ Project/app/main.py
@@ -0,0 +1,32 @@
+from typing import Protocol
+from fastapi import FastAPI, HTTPException, Depends, status
+from enum import Enum
+from sqlalchemy import Engine
+from sqlalchemy.orm import Session
+import os
+from jose import jwt
+from dataclasses import dataclass    
+import app.models as models
+from app.database import engine, SessionLocal
+
+
+app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+# No need for db_dependency annotation
+def db_dependency():
+    return Depends(get_db)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the API, made with FastAPI!!"}
