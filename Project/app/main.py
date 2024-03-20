8059f5763b686ff18cc1f258c686d03cddbfa999
AlecV <alec.vangilbergen@student.ucll.be>
2024-03-20 11:14:51 +0100

    Add teacher creation endpoint and rename teacher repository

--- Project/app/main.py
+++ Project/app/main.py
@@ -9,7 +9,8 @@ from dataclasses import dataclass
 import app.models as models
 from app.database import engine, SessionLocal
 from organisationRepo import OrganisationRepository
-from app.schemas import Organisation
+from teacherRepo import TeacherRepository
+from app.schemas import Organisation, Teacher
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
@@ -41,13 +42,24 @@ def db_dependency():
 async def root():
     return {"message": "Welcome to the API, made with FastAPI!!"}
 
-
+#ORGANISATION
 @app.post("/organisation/add")
 async def create_organisation(organisation: Organisation):
     try:
         repo = OrganisationRepository(engine)
         repo.create_organisation(organisation)
         return {"message": "Organisation created successfully"}
+    except Exception as e:
+        # Log the error if needed
+        # logger.error("An error occurred while creating an item: %s", e)
+        raise HTTPException(status_code=500, detail="Internal server error")
+#TEACHER
+@app.post("/teacher/add")
+async def create_teacher(teacher: Teacher):
+    try:
+        repo = TeacherRepository(engine)
+        repo.create_teacher(teacher)
+        return {"message": "Teacher created successfully"}
     except Exception as e:
         # Log the error if needed
         # logger.error("An error occurred while creating an item: %s", e)
