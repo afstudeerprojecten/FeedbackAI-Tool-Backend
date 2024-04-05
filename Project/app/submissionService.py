992cdc35de5155b9ac11c85b5a41f81a0338e913
Sandeep Singh <sandeep159@outlook.com>
2024-04-05 15:57:05 +0200

    Added add submission to service

--- Project/app/submissionService.py
+++ Project/app/submissionService.py
@@ -19,3 +19,14 @@ class SubmissionService:
     def __init__(self, session: AsyncSession):
         self.session = session
         self.submission_repo = SubmissionRepository(session=self.session)
+
+    async  def __add_submission(self, submision: CreateSubmissionSchema) -> SubmissionModel:
+        submission_repo = SubmissionRepository(session=self.session)
+        new_submission = await submission_repo.create_submission(submision, eager_load=True)
+        return new_submission
+    
+    async def student_submit_assignment(self, submission: CreateSubmissionSchema):
+
+        # create de submision
+        submission = await self.__add_submission(submission)
+        
