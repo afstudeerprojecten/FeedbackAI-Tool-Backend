6fef90e0b9ae790f2302dc9f347596cde0edf83c
AlecV <alec.vangilbergen@student.ucll.be>
2024-04-18 14:27:27 +0200

    Added Sphinx and initial documentation

---
+++ Project/docs/source/conf.py
@@ -0,0 +1,34 @@
+import os
+import sys
+
+# Add the directory containing your project to the Python path
+sys.path.insert(0, os.path.abspath('../../'))
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'FeedbackAI-Tool-API'
+copyright = '2024, Alec'
+author = 'Alec'
+release = '1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
