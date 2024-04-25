cbcc39ec4b7d2dc6cd507dccee742d42f4fef8d3
Sandeep Singh <sandeep159@outlook.com>
2024-04-25 15:24:16 +0200

    Created CourseRepository Interface

---
+++ app/Course/Repository/courseRepositoryInterface.py
@@ -0,0 +1,22 @@
+from app.models import Course
+from app.schemas import CreateCourse, Course as CourseSchema
+from typing import List, Optional, Protocol
+
+
+class ICourseRepository(Protocol):
+    async def create_course(self, course: CreateCourse) -> Course:
+        ...
+
+    async def get_courses(self) -> List[CourseSchema]:
+        ...
+    
+    async def get_course_by_name(self, name: str) -> Optional[CourseSchema]:
+        ...
+    
+    async def get_course_by_id(self, course_id: int) -> Optional[CourseSchema]:
+        ...
+    
+    async def delete_course_by_id(self, course_id: int) -> None:
+        ...
+
+    
