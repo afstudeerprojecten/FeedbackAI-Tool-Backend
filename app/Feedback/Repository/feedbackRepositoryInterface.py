1e069d693f31d1069b66fcaa4622cb849f3d42fe
Sandeep Singh <sandeep159@outlook.com>
2024-04-26 06:36:41 +0200

    Create feedbackRepositoryInterface

---
+++ app/Feedback/Repository/feedbackRepositoryInterface.py
@@ -0,0 +1,12 @@
+from typing import Protocol
+from app.schemas import CreateFeedback as CreateFeedbackSchema
+from app.schemas import Feedback as FeedbackSchema
+
+
+class IFeedbackRepository(Protocol):
+    
+    async def create_feedback(self, feedback: CreateFeedbackSchema) -> FeedbackSchema:
+        ...
+    
+    async def get_feedback_by_submission_id(self, submission_id: int) -> FeedbackSchema:
+        ...
