94ab032c4c18d2f461b777b7f9075f0cd1899faa
Sandeep Singh <sandeep159@outlook.com>
2024-04-26 18:49:32 +0200

    Fix optional for returntype for feedbackbysubmissionid

--- app/Feedback/Repository/feedbackRepositoryInterface.py
+++ app/Feedback/Repository/feedbackRepositoryInterface.py
@@ -1,4 +1,4 @@
-from typing import Protocol
+from typing import Optional, Protocol
 from app.schemas import CreateFeedback as CreateFeedbackSchema
 from app.schemas import Feedback as FeedbackSchema
 
@@ -8,5 +8,5 @@ class IFeedbackRepository(Protocol):
     async def create_feedback(self, feedback: CreateFeedbackSchema) -> FeedbackSchema:
         ...
     
-    async def get_feedback_by_submission_id(self, submission_id: int) -> FeedbackSchema:
+    async def get_feedback_by_submission_id(self, submission_id: int) -> Optional[FeedbackSchema]:
         ...
