201271103aabe18faa3685aa7da5d63a17107b99
Sandeep Singh <sandeep159@outlook.com>
2024-04-26 07:19:41 +0200

    Add create and get by id methods to FeedbackService

--- app/Feedback/Service/feedbackService.py
+++ app/Feedback/Service/feedbackService.py
@@ -18,3 +18,10 @@ class FeedbackService:
         feedbackRepository = FeedbackRepositoryAsync(session=session)
 
         return FeedbackService(feedbackRepository=feedbackRepository)
+
+
+    async def create_feedback(self, feedback: CreateFeedbackSchema) -> FeedbackSchema:
+        return await self.feedbackRepository.create_feedback(feedback=feedback)
+    
+    async def get_feedback_by_submission_id(self, submission_id: int) -> FeedbackSchema:
+        return await self.feedbackRepository.get_feedback_by_submission_id(submission_id=submission_id)
