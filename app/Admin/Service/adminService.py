0dd2c4dcfd31cb784b6ee6a6e7c49a460c68787e
Jesse Storms <jessestorms3@gmail.com>
2024-06-06 10:00:03 +0000

    [#37] added generic exceptions

--- app/Admin/Service/adminService.py
+++ app/Admin/Service/adminService.py
@@ -5,6 +5,7 @@ from app.Admin.Repository.adminRepositoryInterface import IAdminRepository
 from sqlalchemy.ext.asyncio import AsyncSession
 from dataclasses import dataclass
 from app.Admin.Repository.adminRepositoryInterface import IAdminRepository
+from app.exceptions import EntityAlreadyExistsException, EntityNotFoundException
 from app.models import Admin
 from app.schemas import CreateAdmin, Admin as AdminSchema
 from sqlalchemy.ext.asyncio import AsyncEngine
@@ -15,21 +16,21 @@ from typing import List
 from typing import Optional
 
 
-class AdminAlreadyExistsException(Exception):
-    def __init__(self, username: str):
-        self.username = username
+# class AdminAlreadyExistsException(Exception):
+#     def __init__(self, username: str):
+#         self.username = username
 
-class AdminNotFoundException(Exception):
-    def __init__(self, username: str):
-        self.username = username
+# class AdminNotFoundException(Exception):
+#     def __init__(self, username: str):
+#         self.username = username
 
-class AdminIdNotFoundException(Exception):
-    def __init__(self, admin_id: int):
-        self.admin_id = admin_id
+# class AdminIdNotFoundException(Exception):
+#     def __init__(self, admin_id: int):
+#         self.admin_id = admin_id
 
-class NoAdminsFoundException(Exception):
-    def __init__(self):
-        pass
+# class NoAdminsFoundException(Exception):
+#     def __init__(self):
+#         pass
 
 @dataclass
 class AdminService:
@@ -43,20 +44,20 @@ class AdminService:
     
     async def create_admin(self, admin: CreateAdmin) -> Admin:
         if await self.adminRepository.get_admin_by_usernameCheck(admin.username):
-            raise AdminAlreadyExistsException(admin.username)
+            raise EntityAlreadyExistsException(f"Admin with username {admin.username} already exists")
         else:
             await self.adminRepository.create_admin(admin=admin)
             return {"message": "Admin created successfully"}
     
     async def get_admins(self) -> List[AdminSchema]:
         if await self.adminRepository.get_admins() == []:
-            raise NoAdminsFoundException()
+            raise EntityNotFoundException("No admins found")
         else:
             return await self.adminRepository.get_admins()
         
     async def get_admin_by_id(self, admin_id: int) -> Optional[AdminSchema]:
         if not await self.adminRepository.get_admin_by_id(admin_id):
-            raise AdminIdNotFoundException(admin_id)
+            raise EntityNotFoundException(f"Admin with ID {admin_id} not found")
         else:
             return await self.adminRepository.get_admin_by_id(admin_id=admin_id)
     
@@ -65,17 +66,17 @@ class AdminService:
         if admin:
             return admin
         else:
-            None
+            return None
         
     async def get_admin_by_username(self, username: str) -> Optional[AdminSchema]:
         if await self.adminRepository.get_admin_by_usernameCheck(username):
             return await self.adminRepository.get_admin_by_usernameCheck(username)
         else:
-            return AdminNotFoundException(username)
+            raise EntityNotFoundException(f"Admin with username {username} not found")
     
     async def delete_admin_by_id(self, admin_id: int) -> None:
         if not await self.adminRepository.get_admin_by_id(admin_id):
-            raise AdminIdNotFoundException(admin_id)
+            raise EntityNotFoundException(f"Admin with ID {admin_id} not found")
         else:
             await self.adminRepository.delete_admin_by_id(admin_id=admin_id)
             return {"message": "Admin deleted successfully"}
--- app/Events/Service/eventService.py
+++ app/Events/Service/eventService.py
@@ -1,24 +1,25 @@
+from app.exceptions import EntityAlreadyExistsException, EntityNotFoundException
 from app.models import Event
 from app.schemas import CreateEvent, Event as EventSchema
 from app.Events.Repository.eventRepo import Event, InterfaceEventRepository
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
-class EventAlreadyExistsException(Exception):
-    def __init__(self, name: str):
-        self.name = name
+# class EventAlreadyExistsException(Exception):
+#     def __init__(self, name: str):
+#         self.name = name
 
-class EventNotFoundException(Exception):
-    def __init__(self, name: str):
-        self.name = name
+# class EventNotFoundException(Exception):
+#     def __init__(self, name: str):
+#         self.name = name
 
-class EventIdNotFoundException(Exception):
-    def __init__(self, Event_id: int):
-        self.Event_id = Event_id
+# class EventIdNotFoundException(Exception):
+#     def __init__(self, Event_id: int):
+#         self.Event_id = Event_id
 
-class NoEventsFoundException(Exception):
-    def __init__(self):
-        pass
+# class NoEventsFoundException(Exception):
+#     def __init__(self):
+#         pass
 
 class EventService():
     def __init__(self, Event_repo: InterfaceEventRepository):
@@ -26,36 +27,35 @@ class EventService():
         
     async def create_Event(self, Event: CreateEvent):
         if await self.Event_repo.get_Event_by_name(Event.name):
-            raise EventAlreadyExistsException(Event.name)
+            raise EntityAlreadyExistsException(f"Event with name {Event.name} already exists")
         else:
             await self.Event_repo.create_Event(Event)
             return {"message": "Event created successfully"}
 
-        
     async def get_Events(self):
         if await self.Event_repo.get_Events() == []:
-            raise NoEventsFoundException()
+            raise EntityNotFoundException("No Events found")
         else:
             Events = await self.Event_repo.get_Events()
             return Events
 
     async def get_Event_by_id(self, Event_id: int):
         if not await self.Event_repo.get_Event_by_id(Event_id):
-            raise EventIdNotFoundException(Event_id)
+            raise EntityNotFoundException(f"Event with ID {Event_id} not found")
         else:
             Event = await self.Event_repo.get_Event_by_id(Event_id)
             return Event
         
     async def delete_Event(self, Event_id: int):
         if not await self.Event_repo.get_Event_by_id(Event_id):
-            raise EventIdNotFoundException(Event_id)
+            raise EntityNotFoundException(f"Event with ID {Event_id} not found")
         else:
             await self.Event_repo.delete_Event_by_id(Event_id)
             return {"message": "Event deleted successfully"}
         
     async def get_Event_name_by_id(self, Event_id: int):
         if not await self.Event_repo.get_Event_by_id(Event_id):
-            raise EventIdNotFoundException(Event_id)
+            raise EntityNotFoundException(f"Event with ID {Event_id} not found")
         else:
             Event = await self.Event_repo.get_Event_by_id(Event_id)
             return Event.name
