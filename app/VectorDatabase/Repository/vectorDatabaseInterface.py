3e528b34511ed696c734a40fe1a3be5db0173a3c
Sandeep Singh <sandeep159@outlook.com>
2024-06-10 14:05:47 +0200

    Remove unused imports from vectorDatabaseInterface

--- app/VectorDatabase/Repository/vectorDatabaseInterface.py
+++ app/VectorDatabase/Repository/vectorDatabaseInterface.py
@@ -1,12 +1,8 @@
 from typing import Protocol
 from fastapi import UploadFile
 from app.schemas import Course as CourseSchema, Organisation as OrganisationSchema
-from app.vector_database import PERSISTENT_VECTOR_DB_FOLDER
-from app.vector_database import OLLAMA_NOMIC_EMBEDDING
 
 class IVectorDatabase(Protocol):
-
-
 
     # File_path is a str, meaning the path to the file
     # since depending on provider, the file_path can be a url or a local path 
