7dcf176b5ea9d0accf3e286d48884715e42e6786
Sandeep Singh <sandeep159@outlook.com>
2024-06-10 14:06:30 +0200

    Improved a vectorDbInterface method definition, add return type

--- app/VectorDatabase/Repository/vectorDatabaseInterface.py
+++ app/VectorDatabase/Repository/vectorDatabaseInterface.py
@@ -6,7 +6,7 @@ class IVectorDatabase(Protocol):
 
     # File_path is a str, meaning the path to the file
     # since depending on provider, the file_path can be a url or a local path 
-    async def __checkEmbeddingAlreadyExists(self, file_path: str, organisation: OrganisationSchema, course: CourseSchema):
+    async def __checkEmbeddingAlreadyExists(self, file_path: str, organisation: OrganisationSchema, course: CourseSchema) -> bool:
        ...
 
         
