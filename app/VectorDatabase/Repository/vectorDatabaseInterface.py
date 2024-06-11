f8c98b461eeedf9b19be51e319ea2de76b08b872
Sandeep Singh <sandeep159@outlook.com>
2024-06-11 15:10:17 +0200

    Fix indents in vectorDb interface

--- app/VectorDatabase/Repository/vectorDatabaseInterface.py
+++ app/VectorDatabase/Repository/vectorDatabaseInterface.py
@@ -4,14 +4,15 @@ from app.schemas import Course as CourseSchema, Organisation as OrganisationSche
 
 class IVectorDatabase(Protocol):
 
-    # File_path is a str, meaning the path to the file
-    # since depending on provider, the file_path can be a url or a local path 
-    async def __checkEmbeddingAlreadyExists(self, file_path: str, organisation: OrganisationSchema, course: CourseSchema) -> bool:
+   # File_path is a str, meaning the path to the file
+   # since depending on provider, the file_path can be a url or a local path 
+   async def __checkEmbeddingAlreadyExists(self, file_path: str, organisation: OrganisationSchema, course: CourseSchema) -> bool:
        ...
 
         
-    async def saveEmbeddings(self, file_path: str, organisation: OrganisationSchema, course: CourseSchema) -> None:
+   async def saveEmbeddings(self, file_path: str, organisation: OrganisationSchema, course: CourseSchema) -> None:
        ...
 
-    async def __getUniqueCollectionName(organisation: OrganisationSchema, course: CourseSchema) -> str:
+   async def __getUniqueCollectionName(organisation: OrganisationSchema, course: CourseSchema) -> str:
+       ...
        ...
