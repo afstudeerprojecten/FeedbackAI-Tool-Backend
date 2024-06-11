343d03a5b82645ad10bb589668e4dec74f3e58fd
Sandeep Singh <sandeep159@outlook.com>
2024-06-11 11:18:18 +0200

    Update OpenAIEmbedding generator with embedding function

--- app/Embedding/Generator/openAIEmbeddingGenerator.py
+++ app/Embedding/Generator/openAIEmbeddingGenerator.py
@@ -1,15 +1,17 @@
-
-
-
-from dataclasses import dataclass
-
-from fastapi import UploadFile
-
 from app.Embedding.Generator.embeddingGeneratorInterface import IEmbeddingGenerator
+from dataclasses import dataclass
+from fastapi import UploadFile
+from langchain_openai import OpenAIEmbeddings
+import os
+from app.vector_database import OPENAI_EMBEDDING_MODEL_FALLBACK
+
 
 
 @dataclass
 class OpenAIEmbeddingGenerator(IEmbeddingGenerator):
     
     async def generateEmbeddings(self, file: UploadFile, out_):
-        
+        ... 
+
+    def getEmbeddingFunction(self) -> str:
+        return OpenAIEmbeddings(model=os.getenv("OPENAI_EMBEDDING_MODEL", OPENAI_EMBEDDING_MODEL_FALLBACK))
