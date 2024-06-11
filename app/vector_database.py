0e80d552316b15acdb0f815b2db4e904c420d4cd
Sandeep Singh <sandeep159@outlook.com>
2024-06-11 11:17:46 +0200

    Update vector database configuration file with fallback env values

--- app/vector_database.py
+++ app/vector_database.py
@@ -1,28 +1,24 @@
 import os
 from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.vectorstores import Chroma
-from chromadb import chromadb
-from chromadb import Settings
 import shutil
 
+PERSISTENT_VECTOR_DB_FOLDER  = os.getenv("CHROMA_PERSIST_DIRECTORY")
 
-PERSISTENT_VECTOR_DB_FOLDER  = "./vector_database/"
-
-
+# Dit is enkel nodig voor chromadb lokaal? 
 async def create_persistent_vector_db_folder():
+    print(PERSISTENT_VECTOR_DB_FOLDER)
     ## Create persistent database directory to save vectors in 
-    folder = PERSISTENT_VECTOR_DB_FOLDER 
+    folder = PERSISTENT_VECTOR_DB_FOLDER
     # Create the folder if it doesn't exist
     os.makedirs(folder, exist_ok=True)
-
 
 # Create the embedding model
 OLLAMA_NOMIC_EMBEDDING = OllamaEmbeddings(model="nomic-embed-text", show_progress=True)
 
+UPLOADED_FILES_FOLDER_FALLBACK = "./uploaded_files/"
 
-UPLOADED_FILES_FOLDER = "./uploaded_files/"
-
-
+OPENAI_EMBEDDING_MODEL_FALLBACK = "text-embedding-3-large"
 
 async def reset_vector_db():
     # settings = Settings(allow_reset=True)
