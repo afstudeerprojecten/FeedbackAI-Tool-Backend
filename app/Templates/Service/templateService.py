435f458693f4d582467df2b932aed4ff0c652583
Sandeep Singh <sandeep159@outlook.com>
2024-06-12 05:59:46 +0200

    Added entity checks for RAG to templateService

--- app/Templates/Service/templateService.py
+++ app/Templates/Service/templateService.py
@@ -43,7 +43,21 @@ class TemplateService:
     
 
     async def generate_template_solution(self, assignment_id: int) -> str:
-        return await self.templateGenerator.generate_template_solution(assignment_id=assignment_id)
+        #check of assignment id echt is
+        assignment = await self.assignmentRepository.get_assignment_by_id(assignment_id=assignment_id, eager_load=True)
+        if (not assignment):
+            raise EntityNotFoundException(message=f"Assignment with id {assignment_id} does not exist")
+                # check of de course echt is
+        course_id = await self.assignmentRepository.get_course_id_by_assignment_id(assignment_id=assignment.id)
+        if (not course_id):
+            raise EntityNotFoundException(message=f"Course with id {course_id} that assignment with id {assignment_id} belongs to doesn't exist.")
+        
+        # check of de organisation_id echt is
+        organisation_id = await self.assignmentRepository.get_organisation_id_by_assignment_id(assignment_id=assignment_id)
+        if (not organisation_id):
+            raise EntityNotFoundException(message=f"Organisation with id {organisation_id} that this assignment with id {assignment_id} belongs to does not exist.")
+        
+        return await self.templateGenerator.generate_template_solution(assignment_id=assignment_id, vectorDatabase=self.vectorDatabase, organisation_id=organisation_id, course_id=course_id)
 
 
     async def get_all_templates(self) -> list[TemplateSchema]:
