ddf784294f7058355d164c126a23195154a6d721
Jesse Storms <jesse.storms@student.ucll.be>
2024-06-12 09:44:37 +0200

    Fixed oversights by @SandeepMango159 please just build and run before pushing to dev

--- app/vector_database.py
+++ app/vector_database.py
@@ -3,12 +3,12 @@ from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.vectorstores import Chroma
 import shutil
 
-PERSISTENT_VECTOR_DB_FOLDER  = os.getenv("CHROMA_PERSIST_DIRECTORY")
+PERSISTENT_VECTOR_DB_FOLDER  = os.getenv("CHROMA_PERSIST_DIRECTORY", "./data/persistent_vector_db/")
 
 # Dit is enkel nodig voor chromadb lokaal? 
 async def create_persistent_vector_db_folder():
     print(PERSISTENT_VECTOR_DB_FOLDER)
-    ## Create persistent database directory to save vectors in 
+    ## Create persistent database directory to save vectors in. Defaults ./data/persistent_vector_db/   
     folder = PERSISTENT_VECTOR_DB_FOLDER
     # Create the folder if it doesn't exist
     os.makedirs(folder, exist_ok=True)
@@ -16,7 +16,7 @@ async def create_persistent_vector_db_folder():
 # Create the embedding model
 OLLAMA_NOMIC_EMBEDDING = OllamaEmbeddings(model="nomic-embed-text", show_progress=True)
 
-UPLOADED_FILES_FOLDER_FALLBACK = "./uploaded_files/"
+UPLOADED_FILES_FOLDER_FALLBACK = "./data/uploaded_files/"
 
 OPENAI_EMBEDDING_MODEL_FALLBACK = "text-embedding-3-large"
 
